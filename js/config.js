d685e9349e70212b2559f5af57acd97858f40ea9
gzulmc <gzulmc@gmail.com>
2019-04-11 20:09:02 +0800

    111

--- js/config.js
+++ js/config.js
@@ -1,73 +1,73 @@
-// ConnectyCube application credentials
+// // ConnectyCube application credentials
+// //
+// const CC_CREDENTIALS = {
+//     appId: 19,
+//     authKey: 'JHjjDn53msUDzX3',
+//     authSecret: 'AAnMKH7N3b4xX79'
+// };
 //
-const CC_CREDENTIALS = {
-    appId: 19,
-    authKey: 'JHjjDn53msUDzX3',
-    authSecret: 'AAnMKH7N3b4xX79'
-};
-
-const CC_CONFIG = {
-    // endpoints: {
-    //     api: "",
-    //     chat: ""
-    // },
-    streamManagement: {
-        enable: false
-    },
-    debug: {
-        mode: 1
-    }
-};
-
-var CC_USERS = [
-  {
-    id: 38120,
-    login: "lemma",
-    password: "youaintguessmypwd"
-  },
-  {
-    id: 38121,
-    login: "tor",
-    password: "youaintguessmypwd"
-  },
-  {
-    id: 38122,
-    login: "jaila",
-    password: "youaintguessmypwd"
-  },
-  {
-    id: 38123,
-    login: "toe",
-    password: "youaintguessmypwd"
-  },
-  {
-    id: 38124,
-    login: "axis",
-    password: "youaintguessmypwd"
-  }
-];
-
-var appConfig = {
-    dilogsPerRequers: 15,
-    messagesPerRequest: 50,
-    usersPerRequest: 15,
-    typingTimeout: 3 // 3 seconds
-};
-
-var CONSTANTS = {
-    DIALOG_TYPES: {
-        PRIVATE: 3,
-        GROUPCHAT: 2,
-        PUBLICCHAT: 1
-    },
-    ATTACHMENT: {
-        TYPE: 'image',
-        BODY: '[attachment]',
-        MAXSIZE: 2 * 1000000, // set 2 megabytes,
-        MAXSIZEMESSAGE: 'Image is too large. Max size is 2 mb.'
-    },
-    NOTIFICATION_TYPES: {
-        NEW_DIALOG: '1',
-        UPDATE_DIALOG: '2'
-    }
-};
+// const CC_CONFIG = {
+//     // endpoints: {
+//     //     api: "",
+//     //     chat: ""
+//     // },
+//     streamManagement: {
+//         enable: false
+//     },
+//     debug: {
+//         mode: 1
+//     }
+// };
+//
+// // var CC_USERS = [
+// //   {
+// //     id: 38120,
+// //     login: "lemma",
+// //     password: "youaintguessmypwd"
+// //   },
+// //   {
+// //     id: 38121,
+// //     login: "tor",
+// //     password: "youaintguessmypwd"
+// //   },
+// //   {
+// //     id: 38122,
+// //     login: "jaila",
+// //     password: "youaintguessmypwd"
+// //   },
+// //   {
+// //     id: 38123,
+// //     login: "toe",
+// //     password: "youaintguessmypwd"
+// //   },
+// //   {
+// //     id: 38124,
+// //     login: "axis",
+// //     password: "youaintguessmypwd"
+// //   }
+// // ];
+//
+// var appConfig = {
+//     dilogsPerRequers: 15,
+//     messagesPerRequest: 50,
+//     usersPerRequest: 15,
+//     typingTimeout: 3 // 3 seconds
+// };
+//
+// var CONSTANTS = {
+//     DIALOG_TYPES: {
+//         PRIVATE: 3,
+//         GROUPCHAT: 2,
+//         PUBLICCHAT: 1
+//     },
+//     ATTACHMENT: {
+//         TYPE: 'image',
+//         BODY: '[attachment]',
+//         MAXSIZE: 2 * 1000000, // set 2 megabytes,
+//         MAXSIZEMESSAGE: 'Image is too large. Max size is 2 mb.'
+//     },
+//     NOTIFICATION_TYPES: {
+//         NEW_DIALOG: '1',
+//         UPDATE_DIALOG: '2'
+//     }
+// };
